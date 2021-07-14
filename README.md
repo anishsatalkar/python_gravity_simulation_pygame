1bcf1d97b5ce449cbe5ffa91f74d63c919333010
anishsatalkar <anishsatalkar@gmail.com>
2021-07-15 01:14:31 +0530

    Created a method to render information text on objects, reduced default font size to 12 and updated the README.md.

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # python_gravity_simulation_pygame
 A simple implementation which simulates gravity in Python using pygame.
-There are two files, one which uses pure Python lists and classes and the other uses numpy. Numpy implementation is around 70-80x faster than the pure Python implementation.
+There are two files, one which uses pure Python lists and classes and the other uses numpy. 
+Numpy implementation is around 70-80x faster than the pure Python implementation.
+The idea was to keep this project simple by not populating it with config files and such. So if you
+need to modify some parameters, feel free to directly tweak those in the
+source code.
 
 ## Dependencies
 1. pygame
 2. numpy (for the numpy file)
 
+You can run `pip3 install requirements.txt` to install the dependencies for python3 or 
+`pip install requirements.txt` for python2.
+
 ## Additional Info
-You may delete the part of code which prints the text (mass of each object over it) to make the simulation cleaner.
+You may comment out the part of code which prints the text (mass of each object over it) to 
+make the simulation cleaner.
--- gravity_simulation_numpy.py
+++ gravity_simulation_numpy.py
@@ -4,9 +4,23 @@ import time
 import numpy as np
 import pygame
 
+
+def render_object_display_text(object_mass, fx_total, fy_total,
+                               object_vx, object_vy, object_px, object_py):
+    mass_text = f'M={object_mass}'
+    force_text = f'F=({fx_total.__round__(2)},{fy_total.__round__(2)})'
+    velocity_text = f'V=({object_vx.__round__(2)},{object_vy.__round__(2)})'
+    text_str = f'{mass_text}  {force_text}  {velocity_text}'
+
+    text = font.render(text_str, True, BLUE)
+    textRect.center = (object_px + object_mass + 10, object_py + object_mass + 10)
+
+    screen.blit(text, textRect)
+
+
 G = 6.67408e-11 * 100_000_000  # Otherwise the bodies would not move given the small value of gravitational constant
 NUM_OF_BODIES = 10
-WIDTH = 1200
+WIDTH = 800
 HEIGHT = 800
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -27,7 +41,7 @@ pygame.init()
 size = WIDTH, HEIGHT
 screen = pygame.display.set_mode(size)
 
-font = pygame.font.SysFont('Arial', 16)
+font = pygame.font.SysFont('Arial', 12)
 text = font.render('0', True, BLUE)
 textRect = text.get_rect()
 while True:
@@ -55,6 +69,9 @@ while True:
 
         px[i] = px[i] + vx[i]
         py[i] = py[i] + vy[i]
+
+        # Comment this method call to remove the text information over objects
+        render_object_display_text(m[i], fx_total, fy_total, vx[i], vy[i], px[i], py[i])
 
         pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(px[i], py[i], m[i], m[i]))
     print(time.time() - in_t)
